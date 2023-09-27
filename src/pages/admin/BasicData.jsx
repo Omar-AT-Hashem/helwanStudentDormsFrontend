7900cb78359495f61490cab95660e25951fe31d4
Omar-AT-Hashem <omar.at.hashem@gmail.com>
2023-09-27 14:15:32 +0300

    code recovery

--- src/pages/admin/BasicData.jsx
+++ src/pages/admin/BasicData.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import { API_ROUTE } from "../../config/env.js";
 import SearchForStudents from "../../components/minicomponent/SearchForStudents";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const BasicData = () => {
   // State to store the selected student's data
-  const [selectedStudent, setSelectedStudent] = useState(true);
+  const [selectedStudent, setSelectedStudent] = useState();
+  const [selectedStudentData, setSelectedStudentData] = useState();
+
+  useEffect(() => {
+    axios
+      .post(`${API_ROUTE}/employee/students/get-by-id`, {
+        studentId: selectedStudent,
+      })
+      .then((res) => {
+        return setSelectedStudentData(res.data);
+      })
+      .catch((err) => {
+        if (err.code == "ERR_BAD_REQUEST") {
+          return;
+        }
+        toast.dismiss();
+        return toast("Something went wrong");
+      });
+  }, [selectedStudent]);
+
+  console.log(selectedStudentData);
 
   return (
     <div className="pt-16 flex flex-row w-full h-screen">
@@ -27,7 +50,7 @@ const BasicData = () => {
         <SearchForStudents setSelectedStudent={setSelectedStudent} />
       </div>
       {/* Main content area */}
-      <div className="bg-zinc-900 flex-1">
+      <div className="bg-zinc-900 h-full flex-1">
         {/* Header */}
         <div className="bg-sky-700 w-full h-10 text-fuchsia-50 text-center text-2xl">
           بيانات اساسية - جامعة حلوان
@@ -39,7 +62,10 @@ const BasicData = () => {
             <div className="person-details">
               {/* Student image */}
               <div className="person-image">
-                <img src={selectedStudent.imageUrl} alt={selectedStudent.name} />
+                <img
+                  src={selectedStudent.imageUrl}
+                  alt={selectedStudent.name}
+                />
               </div>
               {/* Student information */}
               <div className="person-info">
@@ -48,15 +74,15 @@ const BasicData = () => {
                 <form>
                   <div className="form-group">
                     <label>Date of Applying:</label>
-                    <p>{selectedStudent['date-of-applying']}</p>
+                    <p>{selectedStudent["date-of-applying"]}</p>
                   </div>
                   <div className="form-group">
                     <label>Student ID:</label>
-                    <span>{selectedStudent["student-id"]}</span>
+                    <span>{selectedStudentData.id}</span>
                   </div>
                   <div className="form-group">
                     <label>National ID:</label>
-                    <p>{selectedStudent["national-id"]}</p>
+                    <p>{selectedStudentData.nationalNumber}</p>
                   </div>
                   <div className="form-group">
                     <label>Email:</label>
@@ -64,7 +90,7 @@ const BasicData = () => {
                   </div>
                   <div className="form-group">
                     <label>Mobile Number:</label>
-                    <p>{selectedStudent["mobile-number"]}</p>
+                    <p>{selectedStudentData.mobile}</p>
                   </div>
                   <div className="form-group">
                     <label>Religion:</label>
@@ -72,27 +98,27 @@ const BasicData = () => {
                   </div>
                   <div className="form-group">
                     <label>Father's Name:</label>
-                    <p>{selectedStudent["father-name"]}</p>
+                    <p>{selectedStudentData.fatherName}</p>
                   </div>
                   <div className="form-group">
                     <label>Father's National ID:</label>
-                    <p>{selectedStudent["father-national-id"]}</p>
+                    <p>{selectedStudentData.fatherNationalNumber}</p>
                   </div>
                   <div className="form-group">
                     <label>Father's Job:</label>
-                    <p>{selectedStudent['father-job']}</p>
+                    <p>{selectedStudent["father-job"]}</p>
                   </div>
                   <div className="form-group">
                     <label>Father's Number:</label>
-                    <p>{selectedStudent['father-number']}</p>
+                    <p>{selectedStudent["father-number"]}</p>
                   </div>
                   <div className="form-group">
                     <label>Guardian's Name:</label>
-                    <p>{selectedStudent['guardian-name']}</p>
+                    <p>{selectedStudent["guardian-name"]}</p>
                   </div>
                   <div className="form-group">
                     <label>Guardian's Relationship:</label>
-                    <p>{selectedStudent['guardian-relationship']}</p>
+                    <p>{selectedStudent["guardian-relationship"]}</p>
                   </div>
                   <div className="form-group">
                     <label>Residence:</label>
@@ -100,7 +126,7 @@ const BasicData = () => {
                   </div>
                   <div className="form-group">
                     <label>Address Details:</label>
-                    <p>{selectedStudent['address-details']}</p>
+                    <p>{selectedStudent["address-details"]}</p>
                   </div>
                   <div className="form-group">
                     <label>College:</label>
@@ -116,34 +142,36 @@ const BasicData = () => {
                   </div>
                   <div className="form-group">
                     <label>Apartment Type:</label>
-                    <p>{selectedStudent['apartment-type']}</p>
+                    <p>{selectedStudent["apartment-type"]}</p>
                   </div>
                   <div className="form-group">
                     <label>Disabled:</label>
-                    <p>{selectedStudent.disabled ? 'Yes' : 'No'}</p>
+                    <p>{selectedStudent.disabled ? "Yes" : "No"}</p>
                   </div>
                   <div className="form-group">
                     <label>Family Abroad:</label>
-                    <p>{selectedStudent['family-abroad'] ? 'Yes' : 'No'}</p>
+                    <p>{selectedStudent["family-abroad"] ? "Yes" : "No"}</p>
                   </div>
                   <div className="form-group">
                     <label>No Food:</label>
-                    <p>{selectedStudent['no-food'] ? 'Yes' : 'No'}</p>
+                    <p>{selectedStudent["no-food"] ? "Yes" : "No"}</p>
                   </div>
                   <div className="form-group">
                     <label>Exempt from Expenses:</label>
-                    <p>{selectedStudent['exempt-from-expenses'] ? 'Yes' : 'No'}</p>
+                    <p>
+                      {selectedStudent["exempt-from-expenses"] ? "Yes" : "No"}
+                    </p>
                   </div>
                   <div className="form-group">
                     <label>Application Status:</label>
-                    <p>{selectedStudent['application-status']}</p>
+                    <p>{selectedStudent["application-status"]}</p>
                   </div>
                   <div className="form-group">
                     <label>Notes:</label>
                     <p>{selectedStudent.notes}</p>
                   </div>
                 </form>
-               </div>
+              </div>
             </div>
           ) : (
             // Display a message if no student is selected
