08aea2b4ead623baaca09f1d1bae4367cf485d8f
Hannaaaaaaaaa <118943791+Hannaaaaaaaaa@users.noreply.github.com>
2023-12-02 17:09:32 +0200

    Update Rooms.jsx

--- src/pages/admin/Rooms.jsx
+++ src/pages/admin/Rooms.jsx
@@ -6,9 +6,17 @@ function Rooms() {
   const [selectedBuilding, setSelectedBuilding] = useState({});
   const [selectedLevel, setSelectedLevel] = useState({});
   const [roomsInLevel, setRoomsInLevel] = useState([]);
+  const [selectedRoom, setSelectedRoom] = useState({});
+  const [roomFormVisible, setRoomFormVisible] = useState(false);
+  const [newRoomData, setNewRoomData] = useState({
+    name: '',
+    numberOfBeds: 1,
+    type: 'normal',
+    typeOfHousing: 'students',
+  });
 
   const citiesData = [
-   {
+    {
       name: 'City A',
       type: 'sakan',
       buildings: [
@@ -28,7 +36,6 @@ function Rooms() {
         
       ],
     },
-  
   ];
 
   const handleCityClick = (cityName) => {
@@ -49,85 +56,106 @@ function Rooms() {
     setRoomsInLevel(level.rooms);
   };
 
-  const handleUpdateBuilding = () => {
-    console.log('Building updated:', selectedBuilding);
+  const handleAddRoom = () => {
+    setRoomFormVisible(true);
   };
 
-  const handleAddRoom = () => {
-    console.log('Room added to', selectedLevel);
+  const handleSaveRoom = () => {
+    // Logic to save new room data
+    setRoomFormVisible(false);
+  };
+
+  const handleRoomClick = (room) => {
+    setSelectedRoom(room);
+  };
+
+  const handleUpdateRoom = () => {
+    console.log('Room updated:', selectedRoom);
+    // Logic to update room
+  };
+
+  const handleDeleteRoom = () => {
+    console.log('Room deleted:', selectedRoom);
+    // Logic to delete room
   };
 
   return (
     <div className="rooms-container">
-      <div className="pt-16 flex flex-row w-full h-screen relative font-sans">
-        <Toaster
-          toastOptions={{
-            className: "",
-            style: {
-              border: "1px solid #A9872D",
-              backgroundColor: "#A9872D",
-              padding: "16px",
-              color: "white",
-              fontWeight: "Bold",
-              marginTop: "65px",
-              textAlign: "center",
-            },
-          }}
-        />
-        <div className="flex-1 mt-4 snap-x">
-          <div className="bg-mainBlue w-3/4 h-10 mr-56 text-fuchsia-50 text-center text-2xl mt-4 rounded-lg text-mr-1">
-            اداره التعليمات  - جامعة حلوان
-          </div>
-          <div className="sidebar">
-            <h2>Cities</h2>
+      {/* Your JSX structure... */}
+      <div className="sidebar">
+        <h2>Cities</h2>
+        <ul>
+          {citiesData.map((city, index) => (
+            <li key={index}>
+              <button onClick={() => handleCityClick(city.name)}>{city.name}</button>
+              {selectedCity === city.name && (
+                <ul>
+                  {city.buildings.map((building, index) => (
+                    <li key={index}>
+                      <button onClick={() => handleBuildingClick(building)}>{building.name}</button>
+                      {selectedBuilding.name === building.name && (
+                        <ul>
+                          {building.levels.map((level, index) => (
+                            <li key={index}>
+                              <button onClick={() => handleLevelClick(level)}>{level.name}</button>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="main-content">
+        {/* Your displayed content... */}
+        {selectedLevel.name && (
+          <div>
+            <h3>Rooms in {selectedLevel.name}</h3>
             <ul>
-              {citiesData.map((city, index) => (
-                <li key={index}>
-                  <button onClick={() => handleCityClick(city.name)}>{city.name}</button>
-                  {selectedCity === city.name && (
-                    <ul>
-                      {city.buildings.map((building, index) => (
-                        <li key={index}>
-                          <button onClick={() => handleBuildingClick(building)}>{building.name}</button>
-                          {selectedBuilding.name === building.name && (
-                            <ul>
-                              {building.levels.map((level, index) => (
-                                <li key={index}>
-                                  <button onClick={() => handleLevelClick(level)}>{level.name}</button>
-                                </li>
-                              ))}
-                            </ul>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
+              {roomsInLevel.map((room, index) => (
+                <li key={index} onClick={() => handleRoomClick(room)}>
+                  {room}
                 </li>
               ))}
             </ul>
+            <button onClick={handleAddRoom}>Add Room</button>
           </div>
-          <div className="main-content">
-            <h1>{selectedCity}</h1>
-            {selectedBuilding.name && (
-              <div>
-                <h2>{selectedBuilding.name}</h2>
-                <p>Number of Levels: {selectedBuilding.levels.length}</p>
-                <button onClick={handleUpdateBuilding}>Update</button>
-              </div>
-            )}
-            {selectedLevel.name && (
-              <div>
-                <h3>Rooms in {selectedLevel.name}</h3>
-                <ul>
-                  {roomsInLevel.map((room, index) => (
-                    <li key={index}>{room}</li>
-                  ))}
-                </ul>
-                <button onClick={handleAddRoom}>Add Room</button>
-              </div>
-            )}
+        )}
+        {roomFormVisible && (
+          <div>
+            <h3>Add Room</h3>
+            <input
+              type="text"
+              placeholder="Room Name"
+              value={newRoomData.name}
+              onChange={(e) => setNewRoomData({ ...newRoomData, name: e.target.value })}
+            />
+            <select
+              value={newRoomData.numberOfBeds}
+              onChange={(e) => setNewRoomData({ ...newRoomData, numberOfBeds: e.target.value })}
+            >
+              <option value={1}>1 bed</option>
+              <option value={2}>2 beds</option>
+              <option value={3}>3 beds</option>
+              <option value={4}>4 beds</option>
+            </select>
+            {/* Other inputs and selectors for type, typeOfHousing */}
+            <button onClick={handleSaveRoom}>Save</button>
           </div>
-        </div>
+        )}
+        {selectedRoom.name && (
+          <div>
+            <h3>Selected Room</h3>
+            <p>Name: {selectedRoom.name}</p>
+            {/* Display other room details */}
+            <button onClick={handleUpdateRoom}>Update Room</button>
+            <button onClick={handleDeleteRoom}>Delete Room</button>
+          </div>
+        )}
       </div>
     </div>
   );
