c25cc86aab6b0f664e23446f47abaaa9b736cb06
Omar-AT-Hashem <omar.at.hashem@gmail.com>
2024-01-09 16:30:33 +0200

    student assessment complete

--- src/pages/admin/AssessStudents.jsx
+++ src/pages/admin/AssessStudents.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { API_ROUTE } from "../../config/env.js";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
+import Loading from "../../components/minicomponent/Loading.jsx";
 
 export const AssessStudents = () => {
   if (sessionStorage.getItem("token")) {
@@ -10,13 +11,43 @@ export const AssessStudents = () => {
     ] = `Bearer ${sessionStorage.getItem("token")}`;
   }
 
-  const assess = () => {
+  const [loading, setLoading] = useState(0);
+  const [metaData, setMetaData] = useState();
 
-  }
+  useEffect(() => {
+    setLoading((prev) => prev + 1);
+    axios
+      .get(`${API_ROUTE}/v1/student/meta`)
+      .then((res) => {
+        setLoading((prev) => prev - 1);
+        setMetaData(res.data);
+      })
+      .catch(() => {
+        setLoading((prev) => prev - 1);
+        toast.dismiss();
+        toast("حدث خطا");
+      });
+  }, []);
+
+  const assess = () => {
+    setLoading((prev) => prev + 1);
+    axios
+      .put(`${API_ROUTE}/v1/student/assess-students`)
+      .then(() => {
+        setLoading((prev) => prev - 1);
+        toast.dismiss();
+        toast("تم تنسيق الطلاب بنجاح");
+      })
+      .catch(() => {
+        setLoading((prev) => prev - 1);
+        toast.dismiss();
+        toast("حدث خطا");
+      });
+  };
 
   // State to store the selected student's data
   return (
-    <div className="pt-20 flex flex-row w-full h-screen">
+    <div className="pt-36 flex flex-col gap-10 w-full h-screen items-center">
       <Toaster
         toastOptions={{
           className: "",
@@ -31,10 +62,20 @@ export const AssessStudents = () => {
           },
         }}
       />
+      {loading > 0 && <Loading />}
 
-      <div className="m-auto">
-        <button className="w-40 h-10 bg-green-600 rounded-md hover:opacity-70 transition-all duration-200  hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500"
-        onClick={assess} 
+      {metaData && (
+        <div className="flex flex-col">
+          <span>عدد الطلاب المتقدمين:{metaData.applicantMalesCount}</span>
+          <span>عدد الطالبات المتقدمات:{metaData.applicantFemalesCount}</span>
+          <span>عدد الطلاب الاجمالي:{metaData.totalApplicants}</span>
+        </div>
+      )}
+
+      <div className="">
+        <button
+          className="w-40 h-10 bg-green-600 rounded-md hover:opacity-70 transition-all duration-200  hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500"
+          onClick={assess}
         >
           بدأ التنسيق
         </button>
